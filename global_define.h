fb317e002bda8afacb6d2880fea892a162dba886
yangyi <yangyi@is.iscas.ac.cn>
2020-04-04 22:23:40 +0100

    add original files

---
+++ global_define.h
@@ -0,0 +1,32 @@
+#ifndef _GLOBAL_DEFINE_H_
+#define _GLOBAL_DEFINE_H_
+
+#define _TRACER_DEBUG_
+
+#ifdef _TRACER_DEBUG_
+#define     KdPrint(...) printf(__VA_ARGS__)
+#define     KdFPrint(x, ...) fprintf(x, __VA_ARGS__)
+#define     KdFFlush(x) fflush(x)
+#else
+#define     KdPrint(...)
+#define     KdFPrint(x, ...)
+#define     KdFFlush(x)
+#endif
+
+#define     INPUT_PARAMETER
+#define     OUTPUT_PARAMETER
+
+#define     STATUS_SUCCESSFUL           0
+#define     STATUS_FAIL                 -1
+#define     STATUS_ERROR                -2
+
+
+typedef struct _TEST_FILE_HEADER_{
+    char            szMagicWord[4];
+    unsigned int    uiContentLength;
+    unsigned int    uiFileds;
+    unsigned int    uiContentOffset;
+}TEST_FILE_HEADER,*P_TEST_FILE_HEADER,**PP_TEST_FILE_HEADER;
+
+
+#endif
